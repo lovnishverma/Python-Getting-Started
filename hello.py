5e1f1a9f438ccd7553157ab846ae8124c9a01e80
Lovnish Verma <princelv84@gmail.com>
2025-04-11 13:01:58 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+import sys
+
+if __name__=='__main__':
+    print("Hello World, I'm a python script!")
+
+    # Print some info about this version of Python
+    print("\n\nPython version")
+    print (sys.version)
+    print("Version info.")
+    print (sys.version_info)
